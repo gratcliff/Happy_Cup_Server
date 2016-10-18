d7f64607702914576f4c698574d82ae01071c8de
Alec Barlow <abarlow85@gmail.com>
2016-10-18 14:07:07 -0700

    models

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,5 +1,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from ..website.models import Timestamp
 
 # Create your models here.
+
+class Roast(Timestamp):
+
+	name = models.CharField(max_length=48, help_text='Do not ')
+	origin = models.CharField(max)
--- apps/website/models.py
+++ apps/website/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Timestamp(models.Model):
+
+	created_at = models.DateTimeField(auto_now=True)
+	updated_at = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		abstract = True
