878a30e1938c2d330d36f3f9aaf12158ba4a3258
Gabe Ratcliff <gaberatcliff@gmail.com>
2016-11-01 10:47:03 -0700

    db fixes

--- apps/customers/admin.py
+++ apps/customers/admin.py
@@ -1,3 +1,4 @@
+#Customer Admin
 from django.contrib import admin
 
 # Register your models here.
--- apps/customers/models.py
+++ apps/customers/models.py
@@ -1,3 +1,4 @@
+#Customer Model
 from __future__ import unicode_literals
 
 from django.db import models
